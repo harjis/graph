466b9d8ce2cb153c645c4f210ef8db0ca7758e29
Joonas Harjukallio <harjukallio@gmail.com>
2019-07-22 18:08:28 +0300

    Add Graph stories

--- frontend/src/Graph/stories/Graph.stories.js
+++ frontend/src/Graph/stories/Graph.stories.js
@@ -4,22 +4,30 @@ import { storiesOf } from '@storybook/react';
 
 import Background from '../components/Background/Background';
 import Canvas from '../components/Canvas/Canvas';
-import CenteredText from '../components/NodeContent/CenteredText';
 import DotPattern from '../components/DotPattern/DotPattern';
-import Node from '../components/Node/Node';
-import { DEFAULT_NODE_HEIGHT, DEFAULT_NODE_WIDTH } from '../constants/GraphConstants';
-
-import styles from './CustomNode.module.css';
 
 const size = {
   height: 500,
   width: 500
 };
-const nodes = [{ x: 10, y: 10 }, { x: 100, y: 100 }];
-type Props = {
-  customNodeStyles?: string
-};
-const Graph = (props: Props) => (
+const EmptyCanvas = () => (
+  <div>
+    <Canvas height={size.height} width={size.width}>
+      {() => (
+        <text
+          transform={`translate(${size.width / 2}, ${size.height / 2})`}
+          alignmentBaseline="central"
+          dominantBaseline="central"
+          textAnchor="middle"
+        >
+          Just some text
+        </text>
+      )}
+    </Canvas>
+  </div>
+);
+
+const CanvasWithDot = () => (
   <div>
     <Canvas height={size.height} width={size.width}>
       {({ canvasId }) => (
@@ -28,25 +36,20 @@ const Graph = (props: Props) => (
             <DotPattern patternId={canvasId} />
           </defs>
           <Background patternId={canvasId} height={size.height} width={size.width} />
-          {nodes.map(node => (
-            <Node
-              styles={props.customNodeStyles}
-              height={DEFAULT_NODE_HEIGHT}
-              width={DEFAULT_NODE_WIDTH}
-              x={node.x}
-              y={node.y}
-            >
-              <CenteredText nodeHeight={DEFAULT_NODE_HEIGHT} nodeWidth={DEFAULT_NODE_WIDTH}>
-                Node!
-              </CenteredText>
-            </Node>
-          ))}
+          <text
+            transform={`translate(${size.width / 2}, ${size.height / 2})`}
+            alignmentBaseline="central"
+            dominantBaseline="central"
+            textAnchor="middle"
+          >
+            Just some text
+          </text>
         </React.Fragment>
       )}
     </Canvas>
   </div>
 );
 
-storiesOf('Graph', module)
-  .add('Default', () => <Graph />)
-  .add('With custom node styles', () => <Graph customNodeStyles={styles.customStyles} />);
+storiesOf('Graph/Canvas', module)
+  .add('Default', () => <EmptyCanvas />)
+  .add('With DotPattern', () => <CanvasWithDot />);
---
+++ frontend/src/Graph/stories/Node.stories.js
@@ -0,0 +1,52 @@
+// @flow
+import * as React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import Background from '../components/Background/Background';
+import Canvas from '../components/Canvas/Canvas';
+import CenteredText from '../components/NodeContent/CenteredText';
+import DotPattern from '../components/DotPattern/DotPattern';
+import Node from '../components/Node/Node';
+import { DEFAULT_NODE_HEIGHT, DEFAULT_NODE_WIDTH } from '../constants/GraphConstants';
+
+import styles from './CustomNode.module.css';
+
+const size = {
+  height: 500,
+  width: 500
+};
+const nodes = [{ x: 10, y: 10 }, { x: 100, y: 100 }];
+type Props = {
+  customNodeStyles?: string
+};
+const Graph = (props: Props) => (
+  <div>
+    <Canvas height={size.height} width={size.width}>
+      {({ canvasId }) => (
+        <React.Fragment>
+          <defs>
+            <DotPattern patternId={canvasId} />
+          </defs>
+          <Background patternId={canvasId} height={size.height} width={size.width} />
+          {nodes.map(node => (
+            <Node
+              styles={props.customNodeStyles}
+              height={DEFAULT_NODE_HEIGHT}
+              width={DEFAULT_NODE_WIDTH}
+              x={node.x}
+              y={node.y}
+            >
+              <CenteredText nodeHeight={DEFAULT_NODE_HEIGHT} nodeWidth={DEFAULT_NODE_WIDTH}>
+                Node!
+              </CenteredText>
+            </Node>
+          ))}
+        </React.Fragment>
+      )}
+    </Canvas>
+  </div>
+);
+
+storiesOf('Graph/Nodes', module)
+  .add('Default', () => <Graph />)
+  .add('With custom node styles', () => <Graph customNodeStyles={styles.customStyles} />);

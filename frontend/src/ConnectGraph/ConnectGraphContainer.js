5c8f0c68a2cb8e0693fef6f5038671f3b53b073e
Joonas Harjukallio <harjukallio@gmail.com>
2019-07-24 13:24:19 +0300

    Add flow-typed

--- frontend/src/ConnectGraph/ConnectGraphContainer.js
+++ frontend/src/ConnectGraph/ConnectGraphContainer.js
@@ -1,10 +1,9 @@
 // @flow
 import * as React from 'react';
+import type { ContextRouter } from 'react-router-dom';
 
 import ConnectGraph from './components/ConnectGraph/ConnectGraph';
 
-const nodes = [{ x: 10, y: 10 }, { x: 100, y: 100 }];
-export default (props: any) => {
-  console.log(props);
-  return <ConnectGraph nodes={nodes} />;
+export default (props: ContextRouter) => {
+  return <ConnectGraph nodes={[]} />;
 };

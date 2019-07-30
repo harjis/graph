ba73892ac4e53295eb0c8f811a672ab59f0fb686
Joonas Harjukallio <harjukallio@gmail.com>
2019-07-30 14:43:13 +0300

    Add undo

--- frontend/src/ConnectGraph/api/graphs.js
+++ frontend/src/ConnectGraph/api/graphs.js
@@ -3,5 +3,9 @@ import type { Graph } from '../constants/ConnectGraphTypes';
 import { url } from './common';
 
 export function getGraphs(): Promise<Graph[]> {
-  return fetch(`${url}/graphs`).then(resp => resp.json());
+  return fetch(`${url}/graphs`).then(response => response.json());
+}
+
+export function undoGraph(graphId: number) {
+  return fetch(`${url}/graphs/${graphId}/undo`).then(response => response.json());
 }

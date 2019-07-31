7f57111a287c57618f32486eceaf90b29dc22cf9
Joonas Harjukallio <harjukallio@gmail.com>
2019-07-31 09:38:58 +0300

    Separate to nodesReducer

--- frontend/src/ConnectGraph/reducers/graphReducer.js
+++ frontend/src/ConnectGraph/reducers/graphReducer.js
@@ -1,61 +1,25 @@
 // @flow
-import type { Node, NodeAction, Offset } from '../constants/ConnectGraphTypes';
+import nodesReducer, { initialState as nodesInitialState } from './nodesReducer';
+import type { State as NodesState } from './nodesReducer';
+import type { NodeAction } from '../constants/ConnectGraphTypes';
 
-type State = {|
-  draggedNodeId: ?number,
-  error: ?string,
-  isLoaded: boolean,
-  isLoading: boolean,
-  nodeOffset: Offset,
-  nodes: Node[]
+export type State = {|
+  nodes: NodesState
 |};
 export const initialState = {
-  draggedNodeId: null,
-  error: null,
-  isLoaded: false,
-  isLoading: true,
-  nodeOffset: { x: 0, y: 0 },
-  nodes: []
+  nodes: nodesInitialState
 };
 export default function graphReducer(state: State, action: NodeAction): State {
   switch (action.type) {
     case 'NODES/ADD_NODE':
-      return { ...state, nodes: state.nodes.concat(action.node) };
     case 'NODES/FETCH_START':
-      return { ...state, isLoading: true, isLoaded: false };
     case 'NODES/FETCH_ERROR':
-      return { ...state, isLoading: false, isLoaded: false, error: action.error };
     case 'NODES/FETCH_SUCCESS':
-      return { ...state, isLoading: false, isLoaded: true, nodes: action.nodes };
     case 'NODES/START_DRAG':
-      return { ...state, draggedNodeId: action.nodeId, nodeOffset: action.nodeOffset };
-
-    case 'NODES/DRAG': {
-      if (state.draggedNodeId === null) return state;
-      const xDiff = state.nodeOffset.x - action.pageX;
-      const yDiff = state.nodeOffset.y - action.pageY;
-      return {
-        ...state,
-        nodeOffset: { x: action.pageX, y: action.pageY },
-        nodes: state.nodes.map(node => {
-          if (state.draggedNodeId === node.id) {
-            return {
-              ...node,
-              x: node.x - xDiff,
-              y: node.y - yDiff
-            };
-          } else {
-            return node;
-          }
-        })
-      };
-    }
+    case 'NODES/DRAG':
     case 'NODES/STOP_DRAG':
-      return {
-        ...state,
-        draggedNodeId: initialState.draggedNodeId,
-        nodeOffset: initialState.nodeOffset
-      };
+      return { ...state, nodes: nodesReducer(state.nodes, action) };
+
     default:
       return state;
   }

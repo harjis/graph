175a64615db23787cd43097ef8773427aae17231
Joonas Harjukallio <harjukallio@gmail.com>
2019-07-31 08:14:42 +0300

    Add Edge

---
+++ frontend/src/Graph/components/Edge/Edge.js
@@ -0,0 +1,21 @@
+// @flow
+import * as React from 'react';
+
+import { getOriginToOriginBezierPath } from 'Graph/utils/bezier_utils';
+
+import styles from './Edge.module.css';
+
+type Coordinate = { x: number, y: number };
+type Props = {|
+  from: Coordinate,
+  to: Coordinate,
+  styles?: string
+|};
+const Edge = (props: Props) => (
+  <path
+    d={getOriginToOriginBezierPath(props.from, props.to)}
+    className={props.styles || styles.default}
+  />
+);
+
+export default Edge;

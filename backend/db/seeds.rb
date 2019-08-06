4568dc0fcc51fc98fa59d49ac85abaadad90deed
Joonas Harjukallio <harjukallio@gmail.com>
2019-08-06 10:18:24 +0300

    Refac seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -8,46 +8,46 @@
 
 # Default
 #
-graph = Graph.new(name: 'Graph 1')
-node = InputNode.new(name: 'InputNode 1', x: 10, y: 10)
-node2 = InputNode.new(name: 'InputNode 2', x: 200, y: 10)
-node3 = OutputNode.new(name: 'OutputNode 3', x: 200, y: 200)
-graph.nodes = [node, node2, node3]
-graph.save
-
-edge = Edge.new(from_node: node, to_node: node3)
-edge.save
-edge2 = Edge.new(from_node: node2, to_node: node3)
-edge2.save
-
-graph = Graph.new(name: 'Graph 2')
-node21 = InputNode.new(name: 'InputNode 21', x: 10, y: 10)
-node22 = OutputNode.new(name: 'OutputNode 22', x: 20, y: 20)
-graph.nodes = [node21, node22]
-graph.save
-
-edge = Edge.new(from_node: node21, to_node: node22)
-edge.save
+# graph = Graph.new(name: 'Graph 1')
+# node = InputNode.new(name: 'InputNode 1', x: 10, y: 10)
+# node2 = InputNode.new(name: 'InputNode 2', x: 200, y: 10)
+# node3 = OutputNode.new(name: 'OutputNode 3', x: 200, y: 200)
+# graph.nodes = [node, node2, node3]
+# graph.save
+#
+# edge = Edge.new(from_node: node, to_node: node3)
+# edge.save
+# edge2 = Edge.new(from_node: node2, to_node: node3)
+# edge2.save
+#
+# graph = Graph.new(name: 'Graph 2')
+# node21 = InputNode.new(name: 'InputNode 21', x: 10, y: 10)
+# node22 = OutputNode.new(name: 'OutputNode 22', x: 20, y: 20)
+# graph.nodes = [node21, node22]
+# graph.save
+#
+# edge = Edge.new(from_node: node21, to_node: node22)
+# edge.save
 
 # Testing
 #
-# graph = Graph.new(name: 'Graph 1')
-# node1 = Node.new(name: 'Node 1', x: 50, y: 200)
-# node2 = Node.new(name: 'Node 2', x: 300, y: 400)
-#
-# graph.nodes = [node1, node2]
-# graph.save
-# edge = Edge.new(from_node: node1, to_node: node2)
-# edge.save
-#
-#
-# graph2 = Graph.new(name: 'Graph 2')
-# node3 = Node.new(name: 'Node 3', x: 10, y: 10)
-# node4 = Node.new(name: 'Node 4', x: 400, y: 200)
-# graph2.nodes = [node3, node4]
-# graph2.save
-# edge2 = Edge.new(from_node: node3, to_node: node4)
-# edge2.save
-#
-# edge2 = Edge.new(from_node: node4, to_node: node2)
-# edge2.save
+graph = Graph.new(name: 'Graph 1')
+node1 = InputNode.new(name: 'InputNode 1', x: 50, y: 200)
+node2 = OutputNode.new(name: 'OutputNode 1', x: 300, y: 400)
+
+graph.nodes = [node1, node2]
+graph.save
+edge = Edge.new(from_node: node1, to_node: node2)
+edge.save
+
+
+graph2 = Graph.new(name: 'Graph 2')
+node3 = InputNode.new(name: 'InputNode 2', x: 10, y: 10)
+node4 = OutputNode.new(name: 'OutputNode 2', x: 400, y: 200)
+graph2.nodes = [node3, node4]
+graph2.save
+edge2 = Edge.new(from_node: node3, to_node: node4)
+edge2.save
+
+edge2 = Edge.new(from_node: node4, to_node: node2)
+edge2.save

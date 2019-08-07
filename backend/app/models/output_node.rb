1531886809a3da89ea5053460d2de5b1d7852fd3
Joonas Harjukallio <harjukallio@gmail.com>
2019-08-07 09:42:35 +0300

    Whops

--- backend/app/models/output_node.rb
+++ backend/app/models/output_node.rb
@@ -2,8 +2,8 @@ class OutputNode < Node
   validate :has_only_one_output_node
 
   def has_only_one_output_node
-    output_node_count = self.graph.nodes.select { |node| node.type == 'OutputNode' }.count
-    if output_node_count >= 1
+    output_node_count = self.graph.nodes.select { |node| node.type == 'OutputNode' && node.persisted? }.count
+    if output_node_count == 1
       errors.add(:graph, 'Graph can only have one output node')
     end
   end

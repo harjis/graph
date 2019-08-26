6a38de01627953e8aef597c26b25f0a8cf8ba052
Joonas Harjukallio <harjukallio@gmail.com>
2019-08-26 16:35:20 +0300

    hack

--- frontend/src/ConnectGraph/api/common.js
+++ frontend/src/ConnectGraph/api/common.js
@@ -1,5 +1,6 @@
 // @flow
 
-export const url = 'http://localhost:3000/api';
+// export const url = 'http://localhost:3000/api';
+export const url = 'https://afternoon-ocean-51877.herokuapp.com/api';
 
 export const options = {};

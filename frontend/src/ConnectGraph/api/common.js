017a1c599f2946c21de792e810229d38f205681f
Joonas Harjukallio <harjukallio@gmail.com>
2019-08-27 09:58:07 +0300

    wat

--- frontend/src/ConnectGraph/api/common.js
+++ frontend/src/ConnectGraph/api/common.js
@@ -1,6 +1,6 @@
 // @flow
 
-console.log(process.env.BACKEND_URL);
+console.log(process.env);
 export const url = process.env.BACKEND_URL || 'http://localhost:3000/api';
 // export const url = 'https://afternoon-ocean-51877.herokuapp.com/api';
 

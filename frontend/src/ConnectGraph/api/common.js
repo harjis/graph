f5dd305e7a84c1ffe1c6a15e241821cbc221c5d1
Joonas Harjukallio <harjukallio@gmail.com>
2019-08-27 09:49:09 +0300

    Add heroku config var

--- frontend/src/ConnectGraph/api/common.js
+++ frontend/src/ConnectGraph/api/common.js
@@ -1,6 +1,6 @@
 // @flow
 
-export const url = 'http://localhost:3000/api';
+export const url = process.env.BACKEND_URL || 'http://localhost:3000/api';
 // export const url = 'https://afternoon-ocean-51877.herokuapp.com/api';
 
 export const options = {};
